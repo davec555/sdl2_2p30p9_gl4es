533f128e4319d55e589ab06df82cc107e9fd060c
capehill <juha.niemimaki@gmail.com>
2022-01-14 22:09:12 +0200

    Workaround for unfreed signals

--- test/testjoystick.c
+++ test/testjoystick.c
@@ -314,7 +314,7 @@ main(int argc, char *argv[])
     SDL_DestroyRenderer(screen);
     SDL_DestroyWindow(window);
 
-    SDL_QuitSubSystem(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+    SDL_Quit();
 
     return 0;
 }
