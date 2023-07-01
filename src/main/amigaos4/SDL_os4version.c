378ea32963a8bf229802604b1a1cc2be82636ea6
capehill <juha.niemimaki@gmail.com>
2023-07-01 15:38:16 +0300

    Update version to 2.29.0

--- src/main/amigaos4/SDL_os4version.c
+++ src/main/amigaos4/SDL_os4version.c
@@ -20,6 +20,6 @@
 */
 
 #ifdef __AMIGADATE__
-static const char* const version __attribute__ ((used)) = "\0$VER: libSDL2.so 27.0 (" __AMIGADATE__ ")\0";
+static const char* const version __attribute__ ((used)) = "\0$VER: libSDL2.so 29.0 (" __AMIGADATE__ ")\0";
 #endif
 
